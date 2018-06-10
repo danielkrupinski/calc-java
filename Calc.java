2cb9def27f8000072d72bab1ea489bffdb80cea9
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-10 15:11:24 +0200

    Update calculator

--- Calc.java
+++ Calc.java
@@ -1,9 +1,32 @@
-import java.util.*
+import java.util.*;
 
-public class Calc
-{
+public class Calc {
     public static void main(String[] args)
     {
-        
+        System.out.println("Calculator");
+        System.out.println("Enter operation: ");
+
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        char operation = in.next().charAt(0);
+        double b = in.nextDouble();
+
+        switch (operation) {
+        case '+':
+            System.out.println(a + " + " + b + " = " + (a + b));
+            break;
+        case '-':
+            System.out.println(a + " - " + b + " = " + (a - b));
+            break;
+        case '*':
+            System.out.println(a + " * " + b + " = " + (a * b));
+            break;
+        case '/':
+            System.out.println(a + " / " + b + " = " + (a / b));
+            break;
+        default:
+            System.out.println("Unknown operation!");
+            break;
+        }
     }
 }
